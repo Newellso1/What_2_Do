60660f00ed3bec0158d376cc8b322c747ed4b041
Sam <samnewell91@gmail.com>
2023-06-27 18:02:37 +0100

    Input deleted when added to list

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 $('#button').click(function (){
     let toAdd = $('input[name=listItem]').val();
     $('ol').append('<li>' + toAdd + '</li>');
+    $('input[name=listItem]').val('');
 });
+
+$('ol').sortable();
