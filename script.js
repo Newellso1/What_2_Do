87344fd6c0fcacc29b06c8c672047d71e469bd33
Sam <samnewell91@gmail.com>
2023-06-28 18:22:49 +0100

    Added animations and improved jquery

--- script.js
+++ script.js
@@ -2,16 +2,22 @@
 
 // Function to add input to list
 
-$('#button').click(function (){
-    let toAdd = $('input[name=listItem]').val();
-    $('ol').append('<li> ' + toAdd + ' <span><i class="fa-solid fa-circle-xmark"></i></span>' + '</li>');
-    $('input[name=listItem]').val('');
+$('#button').click(function () {
+    if ($('ol li').length < 14) {
+        let toAdd = $('input[name=listItem]').val();
+        $('ol').append('<li> ' + toAdd + ' <span><i class="fa-solid fa-circle-xmark"></i></span>' + '</li>');
+        $('input[name=listItem]').val('');
+    } else {
+        $('#button').css('color', 'rgba(255, 99, 71, 0.877)');
+        $('#trash').effect('shake', {distance: 1});
+    }
 });
 
 // Function to delete whole list
 
-$('#trash').click(function(){
+$('#trash').click(function () {
     $('.listContainer li').remove();
+    $('#button').css('color', 'var(--cerulean)')
 })
 
 // Code to make list sortable (desktop only)
@@ -20,6 +26,10 @@ $('ol').sortable();
 
 // Function to delete individual list items
 
-$('ol').on('click', 'li span', function(){
+$('ol').on('click', 'li span', function () {
     $(this).closest('li').remove();
+    $('#button').css('color', 'var(--cerulean)')
 })
+
+
+
