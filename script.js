b3808718232e0fcf1bfeae1909068f15040ba012
Sam <samnewell91@gmail.com>
2023-06-28 17:33:24 +0100

    Added a button to delete individual list items

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+
+
+// Function to add input to list
+
 $('#button').click(function (){
     let toAdd = $('input[name=listItem]').val();
-    $('ol').append('<li>' + toAdd + '</li>');
+    $('ol').append('<li> ' + toAdd + ' <span><i class="fa-solid fa-circle-xmark"></i></span>' + '</li>');
     $('input[name=listItem]').val('');
 });
+
+// Function to delete whole list
 
 $('#trash').click(function(){
     $('.listContainer li').remove();
 })
+
+// Code to make list sortable (desktop only)
+
 $('ol').sortable();
+
+// Function to delete individual list items
+
+$('ol').on('click', 'li span', function(){
+    $(this).closest('li').remove();
+})
